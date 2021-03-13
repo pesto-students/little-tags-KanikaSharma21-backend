7cc2d9f03f3bab21f4c3d642b0739d366ddcc07d
Kanika <>
2021-03-13 17:47:15 +0530

    iamge

--- routes/products.js
+++ routes/products.js
@@ -27,7 +27,7 @@ router.get("/", async (req, res) => {
         "Lenovo IdeaPad Slim 3i Intel Celeron N4020 15.6-inch HD Thin and Light Laptop (4GB/256GB SSD/Windows 10/Platinum Grey/1.7Kg), 81WQ003LIN",
       category: "electronics",
       image:
-        "https://images.unsplash.com/photo-1603816241776-1997a372648f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1064&q=80",
+        "https://images.unsplash.com/photo-1587614222007-9496626c9e96?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80",
     },
     {
       productId: 2,
