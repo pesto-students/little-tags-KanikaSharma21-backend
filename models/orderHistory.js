96c9bc9e23429d93a640a02733d515688a834edc
renu ganvir <rganvir500@gmail.com>
2021-03-20 00:48:34 +0530

    add order History and Wishlist model

---
+++ models/orderHistory.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const orderHistorySchema = new mongoose.Schema({
+  userId: { type: String },
+  productId: { type: String },
+  address: {
+    name: { type: String },
+    mobile: { type: Number },
+    address1: { type: String },
+    town: { type: String },
+    city: { type: String },
+    state: { type: String },
+    pincode: { type: Number },
+  },
+  totalAmount: {
+    type: Number,
+  },
+  deliveredOn: { type: String },
+  creationDate: {
+    type: Date,
+    default: () => {
+      return new Date();
+    },
+  },
+  insertDate: {
+    type: Number,
+    default: () => {
+      return Math.round(new Date() / 1000);
+    },
+  },
+});
+
+const OrderHistory = mongoose.model("OrderHistory", orderHistorySchema);
+
+function validateOrderHistoryV1Get(orderHistory) {
+  const schema = Joi.object({
+    limit: Joi.number(),
+    offset: Joi.number(),
+  });
+  return schema.validate(orderHistory);
+}
+
+module.exports.OrderHistory = OrderHistory;
+module.exports.validateOrderHistoryV1Get = validateOrderHistoryV1Get;
