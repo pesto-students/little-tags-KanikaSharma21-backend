6f1f1619ad5b20e258a4f5c22dea69daa23eae7f
Kanika <>
2021-04-08 10:38:05 +0530

    add constants file in auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,87 +1,110 @@
 const jwt = require("jsonwebtoken");
+const { MIDDLEWARE_AUTH_CONSTANTS } = require("../config/constants.js");
 const config = require("config");
 const mongoose = require("mongoose");
 const { User } = require("../models/user");
 
-userAuth = async function (req, res, next) {
+const userAuth = async function (req, res, next) {
   if (!config.get("requiresAuth")) return next();
 
   const token = req.header("Authorization");
   if (!token)
-    return res.status(401).send({ statusCode: 401, message: "Failure", data: "Access denied. No token provided" });
+    return res.status(401).send({ statusCode: 401, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.ACCESS_DENIED });
 
-  let user = {};
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.jwtData = decoded;
 
     if (decoded.role === "user") {
-      user = await User.findOne({ _id: decoded.userId });
+      const user = await User.findOne({ _id: decoded.userId });
       if (!user) {
-        return res.status(401).send({ statusCode: 401, message: "Failure", data: "Access denied. No token provided" });
+        return res
+          .status(401)
+          .send({ statusCode: 401, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.INVALID_USER });
       }
 
       req.userData = user;
     } else {
-      return res.status(403).send({ statusCode: 403, message: "Failure", data: "Access denied" });
+      return res
+        .status(403)
+        .send({ statusCode: 403, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.RESOURCE_FORBIDDEN });
     }
 
     next();
   } catch (ex) {
-    res.status(401).send({ statusCode: 401, message: "Failure", data: "Invalid token" });
+    res.status(401).send({ statusCode: 401, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.INVALID_AUTH_TOKEN });
   }
 };
 
-adminAuth = async function (req, res, next) {
+const adminAuth = async function (req, res, next) {
   if (!config.get("requiresAuth")) return next();
 
   const token = req.header("Authorization");
   if (!token)
-    return res.status(401).send({ statusCode: 401, message: "Failure", data: "Access denied. No token provided" });
+    return res.status(401).send({ statusCode: 401, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.ACCESS_DENIED });
 
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.jwtData = decoded;
 
     if (decoded.role !== "admin")
-      return res.status(403).send({ statusCode: 403, message: "Failure", data: "Access denied" });
+      return res
+        .status(403)
+        .send({ statusCode: 403, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.RESOURCE_FORBIDDEN });
 
     next();
   } catch (ex) {
-    res.status(401).send({ statusCode: 401, message: "Failure", data: "Invalid token" });
+    res.status(401).send({ statusCode: 401, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.INVALID_AUTH_TOKEN });
   }
 };
 
-userAdminAuth = async function (req, res, next) {
+const userAdminAuth = async function (req, res, next) {
   if (!config.get("requiresAuth")) return next();
 
   const token = req.header("Authorization");
   if (!token)
     return res
       .status(401)
-      .send({ apiId: req.apiId, statusCode: 401, message: "Failure", data: "Access denied. No token provided" });
+      .send({ apiId: req.apiId, statusCode: 401, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.ACCESS_DENIED });
 
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.jwtData = decoded;
 
     if (decoded.role === "user") {
-      let user = await User.findOne({ _id: mongoose.Types.ObjectId(decoded.userId) });
+      let user = await User.findOne({ _id: decoded.userId });
 
       if (!user) {
-        return res.status(401).send({ statusCode: 401, message: "Failure", data: "Access denied. No token provided" });
+        return res
+          .status(401)
+          .send({ statusCode: 401, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.INVALID_USER });
       }
 
       req.userData = user;
     } else if (decoded.role === "admin") {
       let admin = await User.findOne({ _id: decoded.userId });
+      if (!admin) {
+        return res
+          .status(401)
+          .send({ statusCode: 401, message: "Failure", data: MIDDLEWARE_AUTH_CONSTANTS.INVALID_ADMIN });
+      }
     } else {
-      return res.status(403).send({ apiId: req.apiId, statusCode: 403, message: "Failure", data: "Access denied" });
+      return res.status(403).send({
+        apiId: req.apiId,
+        statusCode: 403,
+        message: "Failure",
+        data: MIDDLEWARE_AUTH_CONSTANTS.RESOURCE_FORBIDDEN,
+      });
     }
 
     next();
   } catch (ex) {
-    res.status(400).send({ apiId: req.apiId, statusCode: 400, message: "Failure", data: "Invalid token" });
+    res.status(401).send({
+      apiId: req.apiId,
+      statusCode: 401,
+      message: "Failure",
+      data: MIDDLEWARE_AUTH_CONSTANTS.INVALID_AUTH_TOKEN,
+    });
   }
 };
 
